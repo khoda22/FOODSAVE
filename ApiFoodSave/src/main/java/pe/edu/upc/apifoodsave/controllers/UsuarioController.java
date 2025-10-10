7e3eea85bd6e6d2aef3d6441318783fa64525fac
Ximena <u202111698@upc.edu.pe>
2025-10-10 18:32:22 -0500

    agregar queries de inventario y producto

--- ApiFoodSave/src/main/java/pe/edu/upc/apifoodsave/controllers/UsuarioController.java
+++ ApiFoodSave/src/main/java/pe/edu/upc/apifoodsave/controllers/UsuarioController.java
@@ -3,6 +3,7 @@ package pe.edu.upc.apifoodsave.controllers;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 import pe.edu.upc.apifoodsave.dtos.UsernameSinPasswordDTO;
 import pe.edu.upc.apifoodsave.dtos.UsuarioDTO;
